9863d59a7d35ceff8d339eb9481a4525048dde70
Nikely Fedechen <fedechenikely@gmail.com>
2024-12-12 14:21:37 -0300

    feat: adiciona regra para ocultar containers de conteúdo de erro e acerto

---
+++ app.js
@@ -0,0 +1,5 @@
+const containerErro = document.querySelector('.container__conteudo-errou');
+containerErro.style.display = 'none';
+
+const containerAcerto = document.querySelector('.container__conteudo-acertou');
+containerAcerto.style.display = 'none';
